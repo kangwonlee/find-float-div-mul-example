0bc12b19b49346d2f8c74ebefaa6dfecc86ccba8
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2021-02-03 11:54:45 +0900

    Create float_div_mul.py
    find a floating point division and multiplication roundoff example

---
+++ float_div_mul.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+
+def main():
+  num_r = np.arange(1, 20+1)
+  den_r = num_r
+
+  num, den = np.meshgrid(num_r, den_r)
+  num = num.flatten()
+  den = den.flatten()
+
+  div = num / den
+  mul_den = div * den
+  df = pd.DataFrame(
+    index=['num', 'den', 'div', 'mul'],
+    data=[
+        num, den, div, mul_den,
+    ]
+  ).T
+
+  df['div str'] = df['div'].astype('str').str.strip('0')
+  df['div len'] = df['div str'].apply(len)
+  df['mul str'] = df['mul'].astype('str').str.strip('0')
+  df['mul len'] = df['mul str'].apply(len)
+  
+  return df.sort_values('mul len', ascending=False)
+
+if "__main__" == __name__:
+  main()
